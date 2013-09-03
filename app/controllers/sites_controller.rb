ce7d8cc96d0d9ffa175c8ee156bb19921a60f51a
Ken <KenIovino@gmail.com>
2013-09-03 14:33:35 -0400

    Fixed incorrect redirect after update

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -42,7 +42,7 @@ class SitesController < ApplicationController
 
     respond_to do |format|
       if @site.update_attributes(params[:site])
-        format.html { redirect_to @site, notice: 'Site was successfully updated.' }
+        format.html { redirect_to edit_site_path(@site), notice: 'Site was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
